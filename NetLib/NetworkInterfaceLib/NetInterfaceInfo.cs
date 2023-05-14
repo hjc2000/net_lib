8a02d710bbe15fbac9e7b842e2ad0024ec729e1b
hjc2000 <hjc.666@foxmail.com>
2023-05-14 13:35:32 +0800

    1

---
+++ NetLib/NetworkInterfaceLib/NetInterfaceInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace NetLib.NetworkInterfaceLib
+{
+	/// <summary>
+	/// 储存单个网卡的信息。
+	/// 1. 描述
+	/// 2. 非广播地址
+	/// 3. 广播地址
+	/// </summary>
+	public class NetInterfaceInfo
+	{
+		public NetInterfaceInfo(string description, string[] unicastIPAddressInformationArr,
+			string[] multicastIPAddressInformationArr)
+		{
+			Description = description;
+			UnicastIPAddressInformationArr = unicastIPAddressInformationArr;
+			MulticastIPAddressInformationArr = multicastIPAddressInformationArr;
+		}
+		public string Description { get; }
+		public string[] UnicastIPAddressInformationArr { get; }
+		public string[] MulticastIPAddressInformationArr { get; }
+	}
+}
---
+++ 测试用控制台/Program.cs
@@ -0,0 +1,12 @@
+﻿using NetworkInterfaceLib;
+
+NetInterfaceInfoCollection netInterfaceInfoCollection = new NetInterfaceInfoCollection();
+foreach (NetInterfaceInfo netInterfaceInfo in netInterfaceInfoCollection)
+{
+	Console.WriteLine(netInterfaceInfo.Description);
+	foreach (string uip in netInterfaceInfo.UnicastIPAddressInformationArr)
+	{
+		Console.WriteLine(uip);
+	}
+	Console.WriteLine();
+}
