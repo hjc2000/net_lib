555240a8567e2ac6dee702aac53d58d3737c2097
hjc2000 <hjc.666@foxmail.com>
2023-07-03 15:27:53 +0800

    添加输出自动递增端口号

---
+++ NetLib/IP/IPConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Numerics;
+
+namespace NetLib.IP;
+public static class IPConverter
+{
+	public static BigInteger ToBigInteger(this IPAddress ip)
+	{
+		return new BigInteger(ip.GetAddressBytes().Reverse().ToArray());
+	}
+
+	public static IPAddress ToIPAddress(this BigInteger ipBigInt)
+	{
+		return new IPAddress(ipBigInt.ToByteArray().Reverse().ToArray());
+	}
+}
